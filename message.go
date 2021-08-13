b39eb982a130bfe4798c2b323590a4d22eeedca7
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2021-08-13 16:20:42 +0200

    message: rework entry parsing

--- message.go
+++ message.go
@@ -7,7 +7,7 @@
 
 package flowdata // import "github.com/mjolnir42/flowdata"
 
-import "github.com/davecgh/go-spew/spew"
+import "encoding/json"
 
 type Message struct {
 	AgentID  string `json:"AgentID"`
@@ -25,121 +25,32 @@ type Header struct {
 
 type Data []kvpair
 
-type kvpair map[string]interface{}
+type kvpair struct {
+	Key   float64         `json:"I"`
+	Value json.RawMessage `json:"V"`
+}
 
 func (m *Message) Convert() <-chan Record {
-	spew.Dump(`flowdata.message.M:`, m)
 	ret := make(chan Record)
 	go func() {
 		for i := range m.DataSets {
-			res := Record{AgentID: m.AgentID}
-			spew.Dump(`flowdata.message.res/before`, res)
+			res := Record{
+				AgentID:        m.AgentID,
+				TcpControlBits: Bitmask(0),
+				TcpFlags:       Flags{},
+			}
 			for _, pair := range m.DataSets[i] {
-				if key, ok := pair[`I`].(uint64); ok {
-					switch key {
-					case octetDeltaCount:
-						if val, ok := pair[`V`].(uint64); ok {
-							res.OctetCount = val
-						}
-					case packetDeltaCount:
-						if val, ok := pair[`V`].(uint64); ok {
-							res.PacketCount = val
-						}
-					case protocolIdentifier:
-						if val, ok := pair[`V`].(uint8); ok {
-							res.ProtocolID = val
-							switch res.ProtocolID {
-							case 1:
-								res.Protocol = `ICMP`
-							case 2:
-								res.Protocol = `IGMP`
-							case 3:
-								res.Protocol = `IPv4`
-							case 6:
-								res.Protocol = `TCP`
-							case 17:
-								res.Protocol = `UDP`
-							case 41:
-								res.Protocol = `IPv6`
-							case 47:
-								res.Protocol = `GRE`
-							case 50:
-								res.Protocol = `ESP`
-							case 51:
-								res.Protocol = `AH`
-							case 58:
-								res.Protocol = `IPv6-ICMP`
-							case 115:
-								res.Protocol = `L2TP`
-							case 132:
-								res.Protocol = `SCTP`
-							case 136:
-								res.Protocol = `UDPLite`
-							case 137:
-								res.Protocol = `MPLS-in-IP`
-							}
-						}
-					case tcpControlBits:
-						if val, ok := pair[`V`].(string); ok {
-							res.TcpControlBits = ParseBitmap(val)
-							res.TcpFlags.FIN = res.TcpControlBits.Check(flagFIN)
-							res.TcpFlags.SYN = res.TcpControlBits.Check(flagSYN)
-							res.TcpFlags.RST = res.TcpControlBits.Check(flagRST)
-							res.TcpFlags.PSH = res.TcpControlBits.Check(flagPSH)
-							res.TcpFlags.ACK = res.TcpControlBits.Check(flagACK)
-							res.TcpFlags.URG = res.TcpControlBits.Check(flagURG)
-							res.TcpFlags.ECE = res.TcpControlBits.Check(flagECE)
-							res.TcpFlags.CWR = res.TcpControlBits.Check(flagCWR)
-							res.TcpFlags.NS = res.TcpControlBits.Check(flagNS)
-						}
-					case sourceTransportPort:
-						if val, ok := pair[`V`].(uint16); ok {
-							res.SrcPort = val
-						}
-					case sourceIPv4Address:
-						if val, ok := pair[`V`].(string); ok {
-							res.SrcAddress = FormatIP(val)
-						}
-					case destinationTransportPort:
-						if val, ok := pair[`V`].(uint16); ok {
-							res.DstPort = val
-						}
-					case destinationIpv4Address:
-						if val, ok := pair[`V`].(string); ok {
-							res.SrcAddress = FormatIP(val)
-						}
-					case ingressInterface:
-						if val, ok := pair[`V`].(uint32); ok {
-							res.IngressIf = val
-						}
-					case egressInterface:
-						if val, ok := pair[`V`].(uint32); ok {
-							res.EgressIf = val
-						}
-					case sourceIPv6Address:
-						if val, ok := pair[`V`].(string); ok {
-							res.SrcAddress = FormatIP(val)
-						}
-					case destinationIPv6Address:
-						if val, ok := pair[`V`].(string); ok {
-							res.SrcAddress = FormatIP(val)
-						}
-					case ipVersion:
-						if val, ok := pair[`V`].(uint8); ok {
-							res.IPVersion = val
-						}
-					case flowDirection:
-						if val, ok := pair[`V`].(uint8); ok {
-							res.FlowDirection = val
-						}
-					case flowStartMilliseconds:
-						if val, ok := pair[`V`].(int64); ok {
-							res.StartMilli = unix2time(val)
-						}
-					case flowEndMilliseconds:
-						if val, ok := pair[`V`].(int64); ok {
-							res.EndMilli = unix2time(val)
-						}
+				switch pair.Key {
+				case octetDeltaCount:
+					res.OctetCount = parseUint64(pair.Value)
+				case packetDeltaCount:
+					res.PacketCount = parseUint64(pair.Value)
+				case protocolIdentifier:
+					res.ProtocolID = parseUint8(pair.Value)
+					if _, ok := ProtocolNameByID[res.ProtocolID]; ok {
+						res.Protocol = ProtocolNameByID[res.ProtocolID]
+					} else {
+						res.Protocol = ProtoNameUnknown
 					}
 				case tcpControlBits:
 					res.TcpControlBits = ParseBitmask(string(pair.Value))
@@ -152,9 +63,39 @@ func (m *Message) Convert() <-chan Record {
 					res.TcpFlags.ECE = res.TcpControlBits.Has(flagECE)
 					res.TcpFlags.CWR = res.TcpControlBits.Has(flagCWR)
 					res.TcpFlags.NS = res.TcpControlBits.Has(flagNS)
+				case sourceTransportPort:
+					res.SrcPort = parseUint16(pair.Value)
+				case sourceIPv4Address:
+					res.SrcAddress = FormatIP(string(pair.Value))
+				case destinationTransportPort:
+					res.DstPort = parseUint16(pair.Value)
+				case destinationIpv4Address:
+					res.DstAddress = FormatIP(string(pair.Value))
+				case ingressInterface:
+					res.IngressIf = parseUint32(pair.Value)
+				case egressInterface:
+					res.EgressIf = parseUint32(pair.Value)
+				case sourceIPv6Address:
+					res.SrcAddress = FormatIP(string(pair.Value))
+				case destinationIPv6Address:
+					res.DstAddress = FormatIP(string(pair.Value))
+				case ipVersion:
+					res.IPVersion = parseUint8(pair.Value)
+				case flowDirection:
+					res.FlowDirection = parseUint8(pair.Value)
+				case exporterIPv4Address:
+					res.ExpIPv4Addr = FormatIP(string(pair.Value))
+				case exporterIPv6Address:
+					res.ExpIPv6Addr = FormatIP(string(pair.Value))
+				case exportingProcessID:
+					res.ExpPID = parseUint32(pair.Value)
+				case flowStartMilliseconds:
+					res.StartMilli = unix2time(parseInt64(pair.Value))
+				case flowEndMilliseconds:
+					res.EndMilli = unix2time(parseInt64(pair.Value))
+				default:
 				}
 			}
-			spew.Dump(`flowdata.message.res/after`, res)
 			ret <- res
 		}
 		close(ret)
